e7d495cfc7d16bad2a93bcbf198ca58e523dc419
Baeerd <1084415787@qq.com>
2019-03-12 14:47:46 +0800

    init project

--- src/main/java/com/app/AppApplication.java
+++ src/main/java/com/app/AppApplication.java
@@ -4,15 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 // exclude无数据库启动
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 @EnableTransactionManagement
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(AppApplication.class);
     }
 }
