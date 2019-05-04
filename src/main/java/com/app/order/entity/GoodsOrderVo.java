94ad9cd05b1b5c6c09b6fc441715f22e2820fb2a
王祥宇 <2691928148@qq.com>
2019-05-04 21:14:49 +0800

    订单模块

---
+++ src/main/java/com/app/order/entity/GoodsOrderVo.java
@@ -0,0 +1,72 @@
+package com.app.order.entity;
+
+import java.util.List;
+
+import com.app.common.entity.AbstractEntity;
+import com.app.goods.entity.Goods;
+
+public class GoodsOrderVo extends AbstractEntity{
+
+	private String creatDt;
+	
+    private String orderNo;
+    
+    private String shopsName;
+    
+    private Double totalPrice;
+    
+    private String orderTypeView;
+    
+    private List<Goods> goodsList;
+
+    
+	public String getOrderTypeView() {
+		return orderTypeView;
+	}
+
+	public void setOrderTypeView(String orderTypeView) {
+		this.orderTypeView = orderTypeView;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public String getShopsName() {
+		return shopsName;
+	}
+
+	public void setShopsName(String shopsName) {
+		this.shopsName = shopsName;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public List<Goods> getGoodsList() {
+		return goodsList;
+	}
+
+	public void setGoodsList(List<Goods> goodsList) {
+		this.goodsList = goodsList;
+	}
+
+	public String getCreatDt() {
+		return creatDt;
+	}
+
+	public void setCreatDt(String creatDt) {
+		this.creatDt = creatDt;
+	}
+	
+	
+}
